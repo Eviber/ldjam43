a9397ac7aa1e4c9ee930d4b2ad1351d150cf46ab
Eclahn <lgdtheo@gmail.com>
2018-12-02 23:08:34 +0100

    zekflop

--- combo_line.lua
+++ combo_line.lua
@@ -1,14 +1,13 @@
 local Component = require "component"
 
-return {
-	function new_combo()
-		local combo_line = Component.new "combo_line"
-		combo_line.line = queue[1].combo
-		combo_line.cursor = 1
-		return combo_line
-	end
-	
-	combo_line_type = {
-		{new_combo}
-	}
+
+function new_combo()
+	local combo_line = Component.new "combo_line"
+	combo_line.line = queue[1].combo
+	combo_line.cursor = 1
+	return combo_line
+end
+
+combo_line_type = {
+	{new_combo}
 }
